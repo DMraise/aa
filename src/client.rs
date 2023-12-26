43e39f82ae41c09587829dd62c343b80734d0c9d
DMraise <maximusnescafe@mail.ru>
2023-12-26 14:05:01 +0300

    sdfg

---
+++ src/client.rs
@@ -0,0 +1,26 @@
+use example::oxion_client::OxionClient;
+use example::HelloRequest;
+
+pub mod example {
+    tonic::include_proto!("example");
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std:error::Error>> {
+    let mut client = OxionClient::connect(
+        "http://[::1]:50051"
+    ).await?;
+
+    let request = tonic::Request::new(
+        HelloRequest {
+            name: "Hfidhifhjsif".to_owned(),
+        }
+    );
+
+    let response = client.send_message(request).await?; 
+
+    println!("RESPONSE={:?}", response);
+    
+    Ok(())
+}
---
+++ src/server.rs
@@ -0,0 +1,42 @@
+use tonic::{transaport::Server, Requestm Responce, Status};
+
+use example::oxion_server::{Oxion, OxionServer};
+use example::{HelloRequest, HelloResponse};
+
+pub mod example {
+    tonic::include_proto!("example");
+}
+
+#[derive(Debug, Default)]
+pub struct OxionService {}
+
+#[tonic::async_trait]
+impl Oxion for OxionService {
+    async fn send_message(
+        &self, 
+        request: Request<HelloRequest>,
+    ) -> Result<Responce<HelloResponse>, Status>> {
+        println!("Got a request: {:?}", request);
+
+        let req = request.get_inner();
+
+        let reply = HelloResponse {
+            message: format!("Sent {}", req.name).into(),
+        };
+
+        Ok(Responce::new(reply));
+    }
+}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let service = OxionService::default();
+
+
+     Server::builder()
+        .add_service(OxionServer::new(service))
+        .serve(addr)
+        .await?;
+    
+    Ok(())
+}
